30910fa12a6d8db29ebcd8b8d9aa41a6af2ea441
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-01-18 19:24:38 +0100

    add methods to CartPage

--- src/main/java/org/presta/pages/cart/CartPage.java
+++ src/main/java/org/presta/pages/cart/CartPage.java
@@ -4,11 +4,14 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import org.presta.pages.base.BasePage;
 import org.presta.pages.checkout.AddressesPage;
-import org.presta.pages.checkout.PersonalInformationPage;
+
+import java.math.BigDecimal;
 
 public class CartPage extends BasePage {
 
     private final Locator proceedToCheckoutBtn = page.getByText("Proceed to checkout");
+    private final Locator emptyCartLabel = page.locator(".js-cart > .no-items");
+    private final Locator totalPrice = page.locator("div[class='cart-summary-line cart-total'] span[class='value']");
 
     public CartPage(Page page) {
         super(page);
@@ -17,5 +20,13 @@ public class CartPage extends BasePage {
     public AddressesPage clickProceedToCheckoutInCart() {
         proceedToCheckoutBtn.click();
         return new AddressesPage(page);
+    }
+
+    public String getEmptyCartLabel() {
+        return emptyCartLabel.innerText();
+    }
+
+    public BigDecimal getTotalPrice() {
+        return getPrice(totalPrice);
     }
 }
