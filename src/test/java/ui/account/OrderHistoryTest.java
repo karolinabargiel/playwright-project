ed35dd651aa88606dc32d4049ddd1a33df4ee19f
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-01-19 19:18:06 +0100

    add method for test

---
+++ src/test/java/ui/account/OrderHistoryTest.java
@@ -0,0 +1,12 @@
+package ui.account;
+
+import base.Pages;
+import org.testng.annotations.Test;
+
+public class OrderHistoryTest extends Pages {
+
+    @Test
+    public void shouldCheckOrderHistory() {
+
+    }
+}
