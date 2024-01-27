bf248f217a4df3d3baed5a882bb3b93484fe6406
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-01-27 22:33:05 +0100

    add class for FooterPage

---
+++ src/main/java/org/presta/pages/homepage/FooterPage.java
@@ -0,0 +1,11 @@
+package org.presta.pages.homepage;
+
+import com.microsoft.playwright.Page;
+import org.presta.pages.base.BasePage;
+
+public class FooterPage extends BasePage {
+
+    public FooterPage(Page page) {
+        super(page);
+    }
+}
